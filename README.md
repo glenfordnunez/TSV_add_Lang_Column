004011cea703391dc948872f986afb56731484d7
Glenford Nuñez <glenfordnunez@gmail.com>
2021-02-21 19:17:47 -0500

    Done Tings

---
+++ README.md
@@ -0,0 +1,9 @@
+Using the pandas DataFrame. I stared using the default CSV package that comes with Python but that got really dumb really fast. 
+
+I was able to distill my original from 50 + lines down to 30+. Also it is easier to read with Pandas. 
+
+This is a quick script to add an additional column to a TSV file that was imported from Fluentcards.com which is a website that translates words from a Kindle Vocabulary Builder database to an Anki flashcard deck. 
+
+I then import the modified file into my ANKI deck to study. 
+
+Originally I was doing this process manually but I knew that I could automate. 
---
+++ main.py
@@ -0,0 +1,33 @@
+import pandas as pd 
+
+def main():
+	
+	# File Location of file imported from Fluentcards.com
+	file_tsv = "/Users/glenfordnunez/Downloads/La Fiesta del Chivo (Spanish Edition)_basic_2_21_2021.tsv"
+	df = pd.read_csv(file_tsv, sep='\t')
+
+
+	def count_num_cols(df):
+		# Sends number of columns into the Matrix
+		return len(df.columns)
+
+	count_num_cols(df)
+
+
+	def create_new_lang_columnt(df, file_tsv, new_lang_abr):
+		df[new_lang_abr] = new_lang_abr
+		df.to_csv(file_tsv, index=False)
+
+	
+	# User Input for language 
+	new_lang = input("What is the target language\n" +"Should be two letter abbreviation"+ " ")
+
+	# Create new column with new language from user input 
+	create_new_lang_columnt(df, file_tsv, new_lang)
+
+	print(new_lang + " " + "Is the new target language")
+
+
+# indicates app
+if __name__ == "__main__":
+	main()

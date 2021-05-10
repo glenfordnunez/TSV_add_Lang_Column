4c8e28bf0c7ff70626deb3d8e0e5d2511315bc07
Glenford Nuñez <glenfordnunez@gmail.com>
2021-05-10 09:15:26 -0400

    Updates

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import pandas as pd
 def main():
 	
 	# File Location of file imported from Fluentcards.com
-	file_tsv = "/Users/glenfordnunez/Downloads/La Fiesta del Chivo (Spanish Edition)_basic_2_21_2021.tsv"
+	file_location = input("Where is the file B?" + " ")
+	file_tsv = file_location
+	#file_tsv = "/Users/glenfordnunez/Downloads/La Fiesta del Chivo (Spanish Edition)_basic_2_21_2021.tsv"
 	df = pd.read_csv(file_tsv, sep='\t')
 
 
@@ -20,12 +22,15 @@ def main():
 
 	
 	# User Input for language 
-	new_lang = input("What is the target language\n" +"Should be two letter abbreviation"+ " ")
+	
+	new_lang = input("What is the target language?\n" +"Should be two letter abbreviation?"+ " ")
 
 	# Create new column with new language from user input 
-	create_new_lang_columnt(df, file_tsv, new_lang)
+	create_new_lang_columnt(df, file_location, new_lang)
 
-	print(new_lang + " " + "Is the new target language")
+
+
+	print(new_lang + " " + "Is the new target language!")
 
 
 # indicates app
